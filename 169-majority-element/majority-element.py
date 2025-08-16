3a5dec41e1f023f61cce1bba60c419960e2f048a
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-08-16 22:25:12 +0300

    Time: 11 ms (28.25%) | Memory: 19.4 MB (19.03%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,19 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return count.most_common()[0][0]
+        # count = Counter(nums)
+        # return count.most_common()[0][0]
+        n = len(nums)
+        countMap = defaultdict(int)
+
+    # Traverse the list and count occurrences using the hash map
+        for num in nums:
+            countMap[num] += 1
         
+        # Check if current element count exceeds n / 2
+            if countMap[num] > n / 2:
+                return num
+
+        # If no majority element is found, return -1
+        return -1
+            
