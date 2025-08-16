4c41d47bf905adccd9d38b7fbd9f0e1756ce2ba9
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-08-16 22:25:53 +0300

    Time: 15 ms (14.21%) | Memory: 19.3 MB (50.73%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -6,14 +6,10 @@ class Solution:
         n = len(nums)
         countMap = defaultdict(int)
 
-    # Traverse the list and count occurrences using the hash map
         for num in nums:
             countMap[num] += 1
         
-        # Check if current element count exceeds n / 2
             if countMap[num] > n / 2:
                 return num
-
-        # If no majority element is found, return -1
         return -1
             
