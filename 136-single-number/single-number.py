0c1739d889b8f28003149429c92910017d46d603
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-08-20 22:27:08 +0300

    Time: 7 ms (32.53%) | Memory: 20.3 MB (6.70%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,5 +1,11 @@
-from functools import reduce  
-from operator import xor
+# from functools import reduce
+# from operator import xor
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-         return reduce(xor, nums)
+        x  = Counter(nums)
+        for item in list(x.items()):
+            if item[1] == 1:
+                return item[0]
+     
+        #  return reduce(xor, nums)
