2747b04033d812917891b30c65f016e6f12ee0c5
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-08-23 22:19:28 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.18%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -9,10 +9,11 @@ class Solution:
                 output.append([1,1])
             else:
                 for j in range(i+1):
+                    prevRow = output[i-1]
                     if j==0 or j==i:
                         singleRow[j] = 1
                     else:
-                        singleRow[j] = output[i-1][j-1] + output[i-1][j]
+                        singleRow[j] = prevRow[j-1] + prevRow[j]
                 output.append(singleRow)
         return output
         
