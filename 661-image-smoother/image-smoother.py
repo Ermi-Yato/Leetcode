5a225950fc34cfe2277a11ab8eeabfa1767f19c5
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-08-28 11:49:07 +0300

    Time: 160 ms (56.08%) | Memory: 18.7 MB (72.66%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,17 +1,17 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        m, n = len(img), len(img[0])
-        out = [[0]*n for _ in range(m)]
+        rows, cols = len(img), len(img[0])
+        result = [[0] * cols for _ in range(rows)]
 
-        for r in range(m):
-            for c in range(n):
-                total = 0
-                cnt = 0
-                for dr in (-1, 0, 1):
-                    for dc in (-1, 0, 1):
-                        rr, cc = r + dr, c + dc
-                        if 0 <= rr < m and 0 <= cc < n:
-                            total += img[rr][cc]
-                            cnt += 1
-                out[r][c] = total // cnt  # floor average
-        return out
+        for row in range(rows):
+            for col in range(cols):
+
+                # we initialize the sum of all cells under consideration and the count of closest cells.
+                total = count = 0
+                for closestRow in range(row-1, row+2):
+                    for closestCol in range(col-1, col+2):
+                        if 0 <= closestRow < rows and 0 <= closestCol < cols:
+                            count += 1
+                            total += img[closestRow][closestCol]
+                result[row][col] = total // count
+        return result
