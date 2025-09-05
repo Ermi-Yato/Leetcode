56dc61db46d411f706181d5245806f8801e16dd2
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-09-05 21:15:20 +0300

    Time: 3 ms (91.36%) | Memory: 62 MB (41.93%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -8,16 +8,30 @@ function romanToInt(s: string): number {
     "D": 500,
     "M": 1000
   }
-    let value = 0
+    let integerValue = 0
+    let previousCharValue = 0
 
-    for (let i = 0; i + 1 < s.length; i++) {
-        const letter = s[i]
-        if (romanInteger[letter] < romanInteger[s[i + 1]]) {
-        value = value - romanInteger[letter]
-        } else {
-        value = value + romanInteger[letter]
+    for (const char of s) {
+        let value = romanInteger[char]
+        integerValue += value
+
+        if (previousCharValue < value) {
+            integerValue -= (2*previousCharValue)
         }
+        previousCharValue = value
     }
-    value = value + romanInteger[s[s.length-1]]
-    return value
+
+    return integerValue
+    // let value = 0
+
+    // for (let i = 0; i + 1 < s.length; i++) {
+    //     const letter = s[i]
+    //     if (romanInteger[letter] < romanInteger[s[i + 1]]) {
+    //     value = value - romanInteger[letter]
+    //     } else {
+    //     value = value + romanInteger[letter]
+    //     }
+    // }
+    // value = value + romanInteger[s[s.length-1]]
+    // return value
 };
