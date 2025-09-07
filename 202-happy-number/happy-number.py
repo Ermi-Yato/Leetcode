795d9967cea454fa6e2d0c0f73125dcb7e1b3247
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-09-07 21:13:09 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (18.20%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        # to track visited numbers after performing the operation
         visited = set()
+
         while n != 1 and n not in visited:
+            # add n to the set
             visited.add(n)
             res = 0
+            # extract the digits and add the square of each digit
             while n > 0:
                 digit = n % 10
                 res += digit**2
