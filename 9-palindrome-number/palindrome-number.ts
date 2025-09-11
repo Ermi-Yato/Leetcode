ade1cb072ab32071f996d1d04f702aeb3339243e
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-09-11 23:56:06 +0300

    Time: 12 ms (21.33%) | Memory: 66.2 MB (7.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.ts
+++ 9-palindrome-number/palindrome-number.ts
@@ -1,7 +1,6 @@
 function isPalindrome(x: number): boolean {
-    let y: string = String(x)
-    let arrY: string[] = y.split("")
-    let reversedY: string[] = arrY.reverse()
-    let reversedStr: string = reversedY.join("")
-    return parseInt(reversedStr) === x
+    if (x < 0) return false;
+
+    let strX = String(x);
+    return strX === strX.split("").reverse().join("");
 };
