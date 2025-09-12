a53efc07f362a0c7f1ff27bafce1a92f5900c3a6
Ermiyas Birhanu <ermiyasbi007@gmail.com>
2025-09-12 23:35:33 +0300

    Time: 6 ms (70.26%) | Memory: 64.9 MB (76.26%) - LeetSync

--- 9-palindrome-number/palindrome-number.ts
+++ 9-palindrome-number/palindrome-number.ts
@@ -1,6 +1,18 @@
 function isPalindrome(x: number): boolean {
-    if (x < 0) return false;
+    // if (x < 0) return false;
 
-    let strX = String(x);
-    return strX === strX.split("").reverse().join("");
+    // let strX = String(x);
+    // return strX === strX.split("").reverse().join("");
+    // Numbers ending in 0 (except 0 itself) are not palindromes
+    if (x < 0 || (x > 0 && x % 10 === 0)) {
+        return false;
+    }
+    // Reverse half of the number and compare
+    let reversedHalf: number = 0;
+    while (reversedHalf < x) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
 };
